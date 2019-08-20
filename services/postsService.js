57318b6f5f3e64746175dba4b511f9720a2e0aff
gabriel37410 <gabriel_37410@yahoo.com>
2019-08-20 20:19:21 -0300

    versão atual

--- services/postsService.js
+++ services/postsService.js
@@ -1,16 +1,25 @@
 var getPosts = function() {
     var posts = [
         {
+            id: 1,
             title: 'Post 1',
             image: 'post1.jpg',
             description: 'Meu primeiro post',
-            body: 'Meu primeiro post blabla'
+            body: 'Meu primeiro post blabla',
         },
         {
+            id: 2,
             title: 'Post 2',
             image: 'post2.jpg',
             description: 'Meu segundo post',
-            body: 'Meu segundo post blabla blabla' 
+            body: 'Meu segundo post blabla blabla',
+        },
+        {
+            id: 3,
+            title: 'Post 3',
+            image: 'post2.jpg',
+            description: 'Meu segundo post',
+            body: 'Meu segundo post blabla blabla',
         },
     ];
 
