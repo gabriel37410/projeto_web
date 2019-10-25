5c752dd8c0a46a654dd2c56199596e06e394f9f2
gabriel37410 <gabriel_37410@yahoo.com>
2019-10-25 01:11:08 -0300

    Autenticação (falta direcionar especificamente)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,8 +12,18 @@ var servicosRouter = require('./routes/servicos');
 var adminPostsRouter = require('./routes/admin/posts');
 var adminProjectsRouter = require('./routes/admin/projects');
 var adminTestimonialsRouter = require('./routes/admin/testimonials');
+var authRouter = require('./routes/auth');
+var verifyAuth = require('./middlewares/authMiddleware');
 
 var app = express();
+
+app.set('trust proxy', 1);
+app.use(session({
+  secret: 'mySecret123',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,12 +36,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/auth', authRouter);
 app.use('/users', usersRouter);
 app.use('/testimonials', testimonialsRouter);
 app.use('/servicos', servicosRouter);
-app.use('/admin/posts', adminPostsRouter);
-app.use('/admin/projects', adminProjectsRouter);
-app.use('/admin/testimonials', adminTestimonialsRouter);
+app.use('/admin/posts', [verifyAuth], adminPostsRouter);
+app.use('/admin/projects', [verifyAuth], adminProjectsRouter);
+app.use('/admin/testimonials', [verifyAuth], adminTestimonialsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
